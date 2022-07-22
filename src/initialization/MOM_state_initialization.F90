02ed1bd33dcdc2b8268aafd9801741275f3d02c1
WenhaoChen89 <96131003+WenhaoChen89@users.noreply.github.com>
2022-07-22 15:01:48 -0400

    Fix ALE Sponge Diagnostics

--- src/initialization/MOM_state_initialization.F90
+++ src/initialization/MOM_state_initialization.F90
@@ -2113,9 +2113,11 @@ subroutine initialize_sponges_file(G, GV, US, use_temperature, tv, u, v, depth_t
       if (use_temperature) then
         allocate(tmp_tr(isd:ied,jsd:jed,nz_data))
         call MOM_read_data(filename, potemp_var, tmp_tr(:,:,:), G%Domain, scale=US%degC_to_C)
-        call set_up_ALE_sponge_field(tmp_tr, G, GV, tv%T, ALE_CSp)
+        call set_up_ALE_sponge_field(tmp_tr, G, GV, tv%S, ALE_CSp, 'temp', &
+               sp_long_name='temperature', sp_unit = 'degC s-1')
         call MOM_read_data(filename, salin_var, tmp_tr(:,:,:), G%Domain, scale=US%ppt_to_S)
-        call set_up_ALE_sponge_field(tmp_tr, G, GV, tv%S, ALE_CSp)
+        call set_up_ALE_sponge_field(tmp_tr, G, GV, tv%S, ALE_CSp, 'salt', &
+               sp_long_name='salinity', sp_unit = 'g kg-1 s-1')
         deallocate(tmp_tr)
       endif
       if (sponge_uv) then
@@ -2138,16 +2140,19 @@ subroutine initialize_sponges_file(G, GV, US, use_temperature, tv, u, v, depth_t
       endif
       ! The remaining calls to set_up_sponge_field can be in any order.
       if ( use_temperature) then
-        call set_up_ALE_sponge_field(filename, potemp_var, Time, G, GV, US, tv%T, ALE_CSp, scale=US%C_to_degC)
-        call set_up_ALE_sponge_field(filename, salin_var, Time, G, GV, US, tv%S, ALE_CSp, scale=US%S_to_ppt)
+        call set_up_ALE_sponge_field(filename, potemp_var, Time, G, GV, US, tv%T, ALE_CSp, &
+               'temp', sp_long_name='temperature', sp_unit = 'degC s-1', scale=US%degC_to_C)
+        call set_up_ALE_sponge_field(filename, salin_var, Time, G, GV, US, tv%S, ALE_CSp, &
+               'salt', sp_long_name='salinity', sp_unit = 'g kg-1 s-1', scale=US%ppt_to_S)
       endif
       if (sponge_uv) then
         filename = trim(inputdir)//trim(state_uv_file)
         call log_param(param_file, mdl, "INPUTDIR/SPONGE_STATE_UV_FILE", filename)
         if (.not.file_exists(filename, G%Domain)) &
              call MOM_error(FATAL, " initialize_sponges: Unable to open "//trim(filename))
-        call set_up_ALE_sponge_vel_field(filename, u_var, filename, v_var, Time, G, GV, US, ALE_CSp, u, v)
-      endif
+        call set_up_ALE_sponge_vel_field(filename, u_var, filename, v_var, Time, G, GV, US, &
+                                         ALE_CSp, u, v, scale=US%m_s_to_L_T)
+        endif
     endif
   endif
 
